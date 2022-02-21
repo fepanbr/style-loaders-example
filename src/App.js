f13bb1683724ffce8521d46a4c36b6b3e873a04b
fepanbr <fepanbr@gmail.com>
2022-02-21 23:17:50 +0900

    initialize

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+// import './App.scss'
+import styles from './App.module.css';
+
+console.log(styles);
+
+// {
+//   App: "App_App__B2Ebb"
+// App-header: "App_App-header__PyRwd"
+// App-link: "App_App-link__XPeK4"
+// App-logo: "App_App-logo__yMrik"
+// App-logo-spin: "App_App-logo-spin__1up6W"
+// logo: "App_logo__7Edbg"
+// }
+
+// css import 방식에 문제점 => css scope가 전역으로 적용되어 오염 되기 쉽다.
+// BEM => 방식으로 오염을 줄인다.
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="header">
+        <img src={logo} className="logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
